9c108e0c3ac1f5d8d49766692309d0d6528183cc
Elif Maralı <elifmarali00@gmail.com>
2023-11-11 00:52:16 +0300

    Jira project updated

---
+++ Jira/src/components/ShowLoadingPage.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import "../App.css";
+function ShowLoadingPage() {
+  return (
+    <div className="LoadingPage">
+      <div>Loading...</div>
+    </div>
+  );
+}
+
+export default ShowLoadingPage;
