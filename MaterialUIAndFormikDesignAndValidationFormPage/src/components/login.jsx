c39eef7ab6664d5c969ee0b165f787bc14d54c1f
Elif Maralı <elifmarali00@gmail.com>
2024-11-16 16:42:17 +0300

    Dev - Login page formik ile update edildi. Validasyonlar tamamlandi.

--- MaterialUIAndFormikDesignAndValidationFormPage/src/components/login.jsx
+++ MaterialUIAndFormikDesignAndValidationFormPage/src/components/login.jsx
@@ -12,16 +12,42 @@ import {
   Typography,
 } from "@mui/material";
 import HttpsOutlinedIcon from "@mui/icons-material/HttpsOutlined";
+import { ErrorMessage, Field, Form, Formik } from "formik";
+import * as Yup from "yup";
+
 const Login = ({ setTab }) => {
+  const initialValues = {
+    username: "",
+    password: "",
+    rememberMe: false,
+  };
   const paperStyle = {
     padding: 20,
-    height: "44vh",
+    height: "48vh",
     width: 320,
     // margin: "20px auto",
   };
   const avatarStyle = {
     backgroundColor: "#1bbd7e",
   };
+
+  const onSubmit = (values, props) => {
+    console.log("values : ", values);
+    setTimeout(() => {
+      props.resetForm();
+      props.setSubmitting(false);
+    }, 2000);
+    console.log(`props : ${JSON.stringify(props, null, 2)}`);
+    console.log(`isSubmitting : ${props.isSubmitting}`);
+  };
+
+  const controllers = Yup.object({
+    username: Yup.string()
+      .email("Enter a valid email address")
+      .required("This is a required field"),
+    password: Yup.string().required("This is a required field"),
+    rememberMe: Yup.boolean().oneOf([true, false]),
+  });
   return (
     <Container
       style={{
@@ -39,34 +65,82 @@ const Login = ({ setTab }) => {
             </Avatar>
             <h2>Sign In</h2>
           </Grid>
-          <TextField
-            label="Username"
-            placeholder="Enter username"
-            fullWidth
-            required
-            variant="filled"
-          />
-          <TextField
-            label="Password"
-            placeholder="Enter password"
-            fullWidth
-            required
-            type="password"
-            variant="filled"
-          />
-          <FormControlLabel
-            control={<Checkbox defaultChecked />}
-            label="Remember Me"
-          />
-          <Button
-            type="submit"
-            color="primary"
-            fullWidth
-            variant="contained"
-            sx={{ margin: "20px 0" }}
+          <Formik
+            initialValues={initialValues}
+            onSubmit={onSubmit}
+            validationSchema={controllers}
           >
-            Sign In
-          </Button>
+            {({
+              values,
+              handleBlur,
+              handleChange,
+              handleSubmit,
+              handleReset,
+              isSubmitting,
+              status,
+              touched,
+              errors,
+            }) => (
+              <Form>
+                <Field
+                  as={TextField}
+                  label="Username"
+                  placeholder="Enter username"
+                  fullWidth
+                  variant="filled"
+                  name="username"
+                  value={values.username}
+                  onChange={handleChange}
+                  helperText={
+                    errors.username && touched.username && errors.username
+                  }
+                  error={errors.username && touched.username}
+                />
+                <Field
+                  as={TextField}
+                  label="Password"
+                  placeholder="Enter password"
+                  fullWidth
+                  type="password"
+                  variant="filled"
+                  name="password"
+                  value={values.password}
+                  onChange={handleChange}
+                  helperText={
+                    errors.password && touched.password && errors.password
+                  }
+                  error={errors.password && touched.password}
+                />
+                <Field
+                  as={FormControlLabel}
+                  control={
+                    <Checkbox
+                      value={values.rememberMe}
+                      onChange={handleChange}
+                      helperText={
+                        errors.rememberMe &&
+                        touched.rememberMe &&
+                        errors.rememberMe
+                      }
+                      error={errors.rememberMe && touched.rememberMe}
+                    />
+                  }
+                  label="Remember Me"
+                  name="rememberMe"
+                />
+                <Button
+                  type="submit"
+                  color="primary"
+                  fullWidth
+                  variant="contained"
+                  sx={{ margin: "20px 0" }}
+                  disabled={isSubmitting}
+                >
+                  {isSubmitting ? `Loading...` : `Sign In`}
+                </Button>
+              </Form>
+            )}
+          </Formik>
           <Link href="#" underline="none">
             Forgot Password?
           </Link>
